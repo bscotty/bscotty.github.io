911d834e4ec9d7da9456b9c505e33f65f722515f
Bryan Scott <bryan@pajato.com>
2017-03-22 21:28:28 -0400

    Starting Homework 6
    # index.html
    * Added link to Homework 6's index.
    
    # Homework6
    * Added all html and javascript files with minor tweaks to formatting.
    * Started toying around with some of the javascript methods for M.

---
+++ Homework6/drawlib1.js
@@ -0,0 +1,35 @@
+let startTime = Date.now() / 1000, time = startTime;
+function drawCanvases(canvases) {
+    window.canvases = canvases;
+    for (let i = 0; i < canvases.length; i++)
+        trackCursor(canvases[i]);
+    setInterval(function () {
+        let i, canvas, context;
+        time = Date.now() / 1000 - startTime;
+        for (i = 0; i < canvases.length; i++)
+            if ((canvas = canvases[i]).update) {
+                context = canvas.getContext('2d');
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                canvas.update(context);
+            }
+    }, 30);
+}
+function trackCursor(canvas) {
+    canvas.cursor = {x: 0, y: 0, z: 0};
+    canvas.setCursor = function (x, y, z) {
+        let r = this.getBoundingClientRect();
+        this.cursor.x = x - r.left;
+        this.cursor.y = y - r.top;
+        if (z !== undefined)
+            this.cursor.z = z;
+    };
+    canvas.onmousedown = function (e) {
+        this.setCursor(e.clientX, e.clientY, 1);
+    };
+    canvas.onmousemove = function (e) {
+        this.setCursor(e.clientX, e.clientY);
+    };
+    canvas.onmouseup = function (e) {
+        this.setCursor(e.clientX, e.clientY, 0);
+    };
+}

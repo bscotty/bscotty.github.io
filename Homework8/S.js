b2721f88a85758e1919adeae023c910d8e89d444
Bryan Scott <bryan@pajato.com>
2017-04-03 13:03:51 -0400

    Tweaking formatting and some minor syntax stuff, and including ways to navigate the three html indices.

--- Homework8/S.js
+++ Homework8/S.js
@@ -1,31 +1,31 @@
-var S = {};
+const S = {};
 
-   S._quad = function(f, u0, v0, u1, v1) {
-      return [
-         f(u0, v0),
-         f(u1, v0),
-         f(u1, v1),
-         f(u0, v1),
-         f(u0, v0)
-      ];
-   }
+S._quad = function (f, u0, v0, u1, v1) {
+    return [
+        f(u0, v0),
+        f(u1, v0),
+        f(u1, v1),
+        f(u0, v1),
+        f(u0, v0)
+    ];
+};
 
-   S.parametricMesh = function(f, nu, nv) {
-      var i, j, u, v, C = [];
-      for (j = 0 ; j < nv ; j++) {
-         v = j / nv;
-         for (i = 0 ; i < nu ; i++) {
+S.parametricMesh = function (f, nu, nv) {
+    let u, v, C = [];
+    for (let j = 0; j < nv; j++) {
+        v = j / nv;
+        for (let i = 0; i < nu; i++) {
             u = i / nu;
-	    C.push(S._quad(f, u, v, u + 1/nu, v + 1/nv));
-         }
-      }
-      return C;
-   }
+            C.push(S._quad(f, u, v, u + 1 / nu, v + 1 / nv));
+        }
+    }
+    return C;
+};
 
-   S.tube = function(u, v) {
-      var theta = 2 * Math.PI * u;
-      return [ Math.cos(theta),
-               Math.sin(theta),
-	       2 * v - 1 ];
-   }
+S.tube = function (u, v) {
+    const theta = 2 * Math.PI * u;
+    return [Math.cos(theta),
+        Math.sin(theta),
+        2 * v - 1];
+};
 

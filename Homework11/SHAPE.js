b1dae29f07d1bf86fd469356e8cefe8c6b3352bf
Bryan Scott <bryan@pajato.com>
2017-05-08 15:52:41 -0400

    Tweaking the texture and some backend stuff.

--- Homework11/SHAPE.js
+++ Homework11/SHAPE.js
@@ -49,6 +49,26 @@ const SHAPE = (function () {
         });
     }
 
+    /* 0 stays constant, phi switches sign each time, 1 switches sign every 2.
+     0: [0,  1,  phi],   4: [ 1,  phi, 0],   8: [ phi, 0,  1],
+     1: [0,  1, -phi],   5: [ 1, -phi, 0],   9: [-phi, 0,  1],
+     2: [0, -1,  phi],   6: [-1,  phi, 0],   10:[ phi, 0, -1],
+     3: [0, -1, -phi],   7: [-1, -phi, 0],   11:[-phi, 0, -1]*/
+    function icos(i, j) {
+        const phi = (1 + Math.sqrt(5)) / 2;
+        let phi1 = (j % 2 === 0) ? phi : -phi;
+        let one = (j < 2) ? 1 : -1;
+
+        switch (i) {
+            case 0:
+                return [0, one, phi1];
+            case 1:
+                return [one, phi1, 0];
+            case 2:
+                return [phi1, 0, one];
+        }
+    }
+
     my.cylinder = function (n) {
         const V = [];
         addDiskVertices(V, n, -1, -1);
@@ -56,6 +76,39 @@ const SHAPE = (function () {
         addDiskVertices(V, n, 1, 1);
         return V;
     };
+
+    my.unitIcosahedron = function () {
+        const v = [];
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 4; j++) {
+                v.push(icos(i, j));
+            }
+        }
+
+        return [
+            [v[0], v[2], v[8]],  // Face 1
+            [v[0], v[8], v[4]],  // Face 2
+            [v[0], v[4], v[6]],  // Face 3
+            [v[0], v[6], v[9]],  // Face 4
+            [v[0], v[9], v[2]],  // Face 5
+            [v[2], v[7], v[5]],  // Face 6
+            [v[2], v[5], v[8]],  // Face 7
+            [v[2], v[9], v[7]],  // Face 8
+            [v[8], v[5], v[10]], // Face 9
+            [v[8], v[10], v[4]], // Face 0
+            [v[10], v[5], v[3]], // Face 11
+            [v[10], v[3], v[1]], // Face 12
+            [v[10], v[1], v[4]], // Face 13
+            [v[1], v[6], v[4]],  // Face 14
+            [v[1], v[3], v[11]], // Face 15
+            [v[1], v[11], v[6]], // Face 16
+            [v[6], v[11], v[9]], // Face 17
+            [v[11], v[3], v[7]], // Face 18
+            [v[11], v[7], v[9]], // Face 19
+            [v[3], v[5], v[7]]   // Face 20
+        ];
+    };
+
 
     return my;
 })();
